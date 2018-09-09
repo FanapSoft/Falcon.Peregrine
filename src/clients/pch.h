5a5df8818e4bbe172e13684f874f47f67133c27d
Pooya Eimandar <Pooya.Eimandar@live.com>
2018-09-09 15:59:34 +0430

    Update CopyRight

--- src/clients/pch.h
+++ src/clients/pch.h
@@ -1,10 +1,10 @@
 /*
-	Project			 : Falcon.Peregrine. Copyright(c) Pooya Eimandar (http://PooyaEimandar.com) . All rights reserved.
-	Source			 : Please direct any bug to https://github.com/PooyaEimandar/Falcon.Peregrine/issues
-	Website			 : http://WolfSource.io
+	Project			 : Falcon.Peregrine. Copyright(c) Fanap (https://fanapsoft.github.io) . All rights reserved.
+	Source			 : Please direct any bug to https://github.com/FanapSoft/Falcon.Peregrine/issues
+	Website			 : https://fanap.ir
 	Name			 : pch.h
 	Description		 : The pre-compiled header
-	Comment          :
+	Comment                  : The Falcon.Peregrine built based on Wolf.Engine Copyright(c) WolfSource.io (https://github.com/WolfSource/Wolf.Engine) 
 */
 
 #include <GLFW/glfw3.h>
